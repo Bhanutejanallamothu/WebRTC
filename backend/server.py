6234a836a9ea5e631aefa54d1e8d19afd18a09f9
Bhanutejanallamothu <163739352+Bhanutejanallamothu@users.noreply.github.com>
2026-01-27 14:32:12 +0530

    Server update

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,17 @@
 import asyncio
+import os
 import websockets
 from signaling_api import signaling_handler
 
 async def main():
+    port = int(os.environ.get("PORT", 8765))
     async with websockets.serve(
         signaling_handler,
         "0.0.0.0",
-        8765
+        port
     ):
-        print("✅ Signaling API running on ws://localhost:8765")
-        await asyncio.Future()  # run forever
+        print(f"Signaling API running on ws://0.0.0.0:{port}")
+        await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(main())
